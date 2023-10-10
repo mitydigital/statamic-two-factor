1e22dc500c96e7caa01c47fc2afff698983acea7
Marty Friedel <1491079+martyf@users.noreply.github.com>
2023-10-11 07:31:16 +1100

    Fix issue on challenge with enter key

--- resources/views/challenge.blade.php
+++ resources/views/challenge.blade.php
@@ -12,8 +12,10 @@
 
                 <div class="pb-4 mb-2 text-center">
                     <h1 class="mb-4 text-lg text-gray-800">{{ __('statamic-two-factor::challenge.title') }}</h1>
-                    <p @if($mode === 'recovery_code') x-cloak @endif x-show="mode === 'code'" class="text-sm text-gray">{{ __('statamic-two-factor::challenge.code_introduction') }}</p>
-                    <p @if($mode === 'code') x-cloak @endif x-show="mode === 'recovery_code'" class="text-sm text-gray">{{ __('statamic-two-factor::challenge.recovery_code_introduction') }}</p>
+                    <p @if($mode === 'recovery_code') x-cloak @endif x-show="mode === 'code'"
+                       class="text-sm text-gray">{{ __('statamic-two-factor::challenge.code_introduction') }}</p>
+                    <p @if($mode === 'code') x-cloak @endif x-show="mode === 'recovery_code'"
+                       class="text-sm text-gray">{{ __('statamic-two-factor::challenge.recovery_code_introduction') }}</p>
                 </div>
 
                 <div>
@@ -26,21 +28,27 @@
                              @if($mode === 'recovery_code') x-cloak @endif
                              x-show="mode === 'code'">
                             <label class="mb-2" for="input-code">{{ __('statamic-two-factor::challenge.code') }}</label>
-                            <input x-model="code" type="text" class="input-text" name="code" pattern="[0-9]*" maxlength="6" inputmode="numeric" autofocus autocomplete="off" id="input-code">
-                            @error('code')<div class="text-red-500 text-xs mt-2">{{ $message }}</div>@enderror
+                            <input x-model="code" type="text" class="input-text" name="code" pattern="[0-9]*"
+                                   maxlength="6" inputmode="numeric" autofocus autocomplete="off" id="input-code">
+                            @error('code')
+                            <div class="text-red-500 text-xs mt-2">{{ $message }}</div>@enderror
                         </div>
 
                         <div class="mb-8"
                              @if($mode === 'code') x-cloak @endif
                              x-show="mode === 'recovery_code'">
-                            <label class="mb-2" for="input-recovery-code">{{ __('statamic-two-factor::challenge.recovery_code') }}</label>
-                            <input x-model="recovery_code" type="text" class="input-text" name="recovery_code" maxlength="21" autofocus autocomplete="off" id="input-recovery-code">
-                            @error('recovery_code')<div class="text-red-500 text-xs mt-2">{{ $message }}</div>@enderror
+                            <label class="mb-2"
+                                   for="input-recovery-code">{{ __('statamic-two-factor::challenge.recovery_code') }}</label>
+                            <input x-model="recovery_code" type="text" class="input-text" name="recovery_code"
+                                   maxlength="21" autofocus autocomplete="off" id="input-recovery-code">
+                            @error('recovery_code')
+                            <div class="text-red-500 text-xs mt-2">{{ $message }}</div>@enderror
                         </div>
 
                         <div class="flex justify-between items-center">
 
                             <button class="text-xs text-btn"
+                                    type="button"
                                     @if($mode === 'recovery_code') x-cloak @endif
                                     x-on:click.prevent="mode = 'recovery_code'; code = ''"
                                     x-show="mode === 'code'">
@@ -48,13 +56,15 @@
                             </button>
 
                             <button class="text-xs text-btn"
+                                    type="button"
                                     @if($mode === 'code') x-cloak @endif
                                     x-on:click.prevent="mode = 'code'; recovery_code = ''"
                                     x-show="mode === 'recovery_code'">
                                 {{ __('statamic-two-factor::challenge.code_use') }}
                             </button>
 
-                            <button type="submit" class="btn-primary">{{ __('statamic-two-factor::challenge.action') }}</button>
+                            <button type="submit"
+                                    class="btn-primary">{{ __('statamic-two-factor::challenge.action') }}</button>
                         </div>
 
                     </form>
@@ -62,7 +72,8 @@
             </div>
 
             <div class="mt-4 text-xs text-center">
-                <a class="logout" href="{{ cp_route('logout') }}?redirect={{ cp_route('login') }}">{{ __('statamic-two-factor::actions.logout') }}</a>
+                <a class="logout"
+                   href="{{ cp_route('logout') }}?redirect={{ cp_route('login') }}">{{ __('statamic-two-factor::actions.logout') }}</a>
             </div>
         </div>
     </div>
