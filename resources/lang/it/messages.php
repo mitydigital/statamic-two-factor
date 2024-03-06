c77d669d3b47f60772b427f0d1fb9d3c383fde64
Davide Prevosto <davide.prevosto@encodia.it>
2024-03-06 10:47:18 +0100

    feat: add italian translations
    This PR add the "it" folder into the lang directory. It provides the italian localization
    of messages and labels.

---
+++ resources/lang/it/messages.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'two_factor_not_setup' => "L'autenticazione a due fattori non è stata configurata per questo account.",
+
+    'code_challenge_failed' => "Il codice utilizzato per l'autenticazione a due fattori non è valido.",
+
+    'recovery_code_challenge_failed' => "Il codice di emergenza utilizzato per l'autenticazione a due fattori non è valido.",
+
+    'recovery_codes_updated' => "Questo codice è stato utilizzato e sostituito con uno nuovo; non dimenticare di aggiornare i tuoi codici di emergenza.",
+
+    'recovery_code_used_greeting' => 'Ciao :name,',
+    'recovery_code_used_subject' => 'È stato utilizzato un codice di emergenza',
+    'recovery_code_used_body' => 'Uno dei tuoi codici di emergenza è stato utilizzato per fare accesso al tuo account. Questo codice non è più valido ed è stato sostituito con uno nuovo.',
+    'recovery_code_used_body_2' => 'Ti invitiamo a visualizzare i tuoi nuovi codici di emergenza nel tuo Profilo e a conservarli in un luogo sicuro.',
+    'recovery_code_used_body_3' => 'Se non sei stato tu, cambia immediatamente la tua password e reimposta l\'autenticazione a due fattori dal tuo Profilo.',
+
+    'recovery_code_used_action' => 'Visualizza Profilo',
+
+    'setup' => "L'autenticazione a due fattori è attiva.",
+
+    'blueprint_tab' => 'Autenticazione a due fattori',
+    'blueprint_field' => 'Autenticazione a due fattori',
+    'blueprint_field_success' => 'Il campo "Autenticazione a due fattori" è stato aggiunto correttamente!',
+];
---
+++ resources/lang/it/setup.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Autenticazione a due fattori',
+    'introduction' => 'Inquadra il QR code (oppure utilizza il codice testuale) e inserisci le 6 cifre generate dalla tua app di autenticazione.',
+
+    'code' => 'Codice',
+
+    'verification_code' => 'Codice di verifica',
+
+    'action' => 'Abilita 2FA',
+];
