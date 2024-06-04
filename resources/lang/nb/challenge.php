94de4f39e2d6de699a94a663f8a6c1b20555c8d2
Håvard Grimelid <havard@mediebruket.no>
2024-06-04 15:36:36 +0200

    Add translation for Norwegian Bokmål

---
+++ resources/lang/nb/challenge.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title' => 'Tofaktorautentisering',
+
+    'code' => 'Kode',
+    'code_introduction' => 'Bekreft tilgang til kontoen din ved å angi autentiseringskoden fra autentiseringsappen.',
+    'code_use' => 'Bruk engangskode',
+
+    'recovery_code' => 'Gjenopprettingskode',
+    'recovery_code_introduction' => 'Bekreft tilgang til kontoen din ved å angi en av gjennopprettingskodene.',
+    'recovery_code_use' => 'Bruk gjenopprettingskode',
+
+    'action' => 'Fortsett',
+];
